3fcd9263f82600d73acf61d70a428ffb127c0e5c
Kos1la <kos1la1998@gmail.com>
2024-06-09 19:49:53 +0300

    Added delete user method

--- UserController.js
+++ UserController.js
@@ -18,6 +18,9 @@ class UserController {
             const updateData = req.body
             console.log(updateData)
             console.log(id)
+            if(!id) {
+                res.status(400).json({message: 'Didnt get id'})
+            }
             const user = await User.findByIdAndUpdate(id,updateData,{new:true})
             res.json(user)
         } catch (e) {
@@ -34,12 +37,23 @@ class UserController {
             res.status(500).json({e:"Get all users error"})
         }
     }
-    async delete(req,res) {
+    async delete(req, res) {
         try {
+            const id = req.params.id;
+            if (!id) {
+                return res.status(400).json({ message: 'ID not provided' });
+            }
 
+            const user = await User.findById(id);
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' }); // сначала находим а потом удаляем чтобы в логах не было null
+            }
+
+            await User.findByIdAndDelete(id);
+            return res.status(200).json({ message: 'User deleted successfully' });
         } catch (e) {
-            console.log(e)
-            res.status(500).json({e: "Delete user error"})
+            console.log(e);
+            res.status(500).json({ error: "Delete user error" });
         }
     }
 }
